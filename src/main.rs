77ec99e5889a9d61f40447404086931561654039
Zac Hooper <zac.g.hooper@gmail.com>
2022-12-08 21:17:42 +1100

    Project Complete

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
+use rand::thread_rng;
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    // generates a locally seeded number between 1 and 100
+    let secret_number = thread_rng().gen_range(1..100);
 
-    let mut guess = String::new();
+    loop {
+        println!("Please input your guess.");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        let mut guess = String::new();
 
-    println!("You guessed: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // parses the guess into a number. If a number is not entered, the program will loop back to the start
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        // Checks to see if the guess is equal to the secret number
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
